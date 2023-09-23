f5e2ad5c8e1784dc3ded42924509b65bb6f51d8f
unknown <nitishrawat009@gmail.com>
2023-09-23 14:07:29 +0530

    tried clicking the captcha

--- login.py
+++ login.py
@@ -3,28 +3,54 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.webdriver.support.ui import Select
+import time
+
+def find_all_iframes(driver):
+    iframes = driver.find_element("xpath","//iframe")
+    for index, iframe in enumerate(iframes):
+        # Your sweet business logic applied to iframe goes here.
+        driver.switch_to.frame(index)
+        find_all_iframes(driver)
+        driver.switch_to.parent_frame()
+
 def bypass_captcha(driver):
     ########################################################
     
 
     #######################################################
-    # Find the captcha element.
-    captcha_element = driver.find_element("id","g-recaptcha-response")
+    print("crash")
 
-    # Create an ActionChains object.
-    action_chains = ActionChains(driver)
+    # back to default web page frame
+    driver.switch_to.default_content()
 
-    # Move the mouse to the captcha element.
-    action_chains.move_to_element(captcha_element)
+    driver.switch_to.frame("/html/body/form/div[3]/div/div[2]/div[2]/div[3]/div[2]/div/span/div/div/div/iframe")
 
-    # Click the captcha element.
-    action_chains.click(captcha_element)
+    # WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,'//*[@id="pnlCaptcha"]/span/div/div/div/iframe')))
+    driver.find_element('xpath', "/html/body/div[2]/div[3]/div[1]/div/div/span").click()
 
-    # Release the mouse button.
-    action_chains.release()
 
-    # Perform the action chains.
-    action_chains.perform()
+    
+    # back to default web page frame
+    driver.switch_to.default_content()
+
+    # # Find the captcha element.
+    # captcha_element = driver.find_element("id","g-recaptcha-response")
+
+    # # Create an ActionChains object.
+    # action_chains = ActionChains(driver)
+
+    # # Move the mouse to the captcha element.
+    # action_chains.move_to_element(captcha_element)
+
+    # # Click the captcha element.
+    # action_chains.click(captcha_element)
+
+    # # Release the mouse button.
+    # action_chains.release()
+
+    # # Perform the action chains.
+    # action_chains.perform()
 
 def login(driver, username, password):
     # Find the username field.
@@ -45,14 +71,20 @@ def login(driver, username, password):
     # Enter the password.
     password_field.send_keys(password)
 
+    print("crash")
+
     # Bypass the captcha.
     bypass_captcha(driver)
 
-    # Find the login button.
-    login_button = driver.find_element("id","btnSearch")
+    print("crash")
 
-    # Click the login button.
-    login_button.click()
+    # # Find the login button.
+    # login_button = WebDriverWait(driver, 20).until(
+    # EC.element_to_be_clickable(("btnSearch", 'enabled_trigger'))
+    # )
+
+    # # Click the login button.
+    # login_button.click()
 
 def main():
     # Create a new Selenium WebDriver instance.
@@ -61,11 +93,13 @@ def main():
     # Open the website.
     driver.get('https://erp.aktu.ac.in/webpages/oneview/oneview.aspx')
 
+    find_all_iframes(driver)
+
     # Login to the website.
     login(driver, '2200970140075', '02-07-2002')
 
-    # Close the WebDriver instance.
-    driver.quit()
+    # # Close the WebDriver instance.
+    # driver.quit()
 
 if __name__ == '__main__':
     main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+    # # Find the login button.
+    # login_button = WebDriverWait(driver, 20).until(
+    # EC.element_to_be_clickable(("btnSearch", 'enabled_trigger'))
+    # )
+
+    # # Click the login button.
+    # login_button.click()
