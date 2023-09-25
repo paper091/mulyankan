01381a72854146c1550bb0f9913ccd497a9ed1f5
unknown <nitishrawat009@gmail.com>
2023-09-25 14:41:36 +0530

    captcha checkbox click implemented taking in consideration frames

--- login.py
+++ login.py
@@ -1,69 +1,92 @@
-# import webdriver
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-# import Action chains 
 from selenium.webdriver.common.action_chains import ActionChains
- 
-# create webdriver object
-driver = webdriver.Firefox()
 
-# for waiting for dynamic html updates
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
- 
- 
-# get geeksforgeeks.org
-url = "https://erp.aktu.ac.in/webpages/oneview/oneview.aspx"
-driver.implicitly_wait(2)
-driver.get(url)
- 
-# get element
-roll_container = driver.find_element(By.ID, "txtRollNo")
-continue_btn = driver.find_element(By.ID, "btnProceed")
 
- 
-# send keys
-roll_container.send_keys("2200970140074")
+def bypass_captcha(driver):
+    ########################################################
+    
 
-# create action chain object
-action = ActionChains(driver)
-action.click(on_element = continue_btn)
-action.perform()
+    #######################################################
+    # a-tcrwswf2nwx2
+    # c-tcrwswf2nwx2
 
-#############
-#  next page
-#############
+    driver.switch_to.default_content()
+    # driver.switch_to.frame('/html/body/form/div[3]/div/div[2]/div[2]/div[3]/div[2]/div/span/div/div/div/iframe')
+    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "/html/body/form/div[3]/div/div[2]/div[2]/div[3]/div[2]/div/span/div/div/div/iframe")))
 
-# driver.refresh()
-# driver.switch_to.frame("content")
-DOB_container = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "txtDOB"))
+    # Find the captcha element.
+    captcha_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "recaptcha-anchor"))
     )
-DOB_container.send_keys("19/10/2001")
 
-div = driver.find_element(By.CLASS_NAME, "container-login100")
-action.click(on_element = div)
-action.perform()
+    # Create an ActionChains object.
+    action_chains = ActionChains(driver)
 
-# recaptcha = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.ID, "recaptcha-anchor"))
-#     )
-# action.click(on_element = recaptcha)
-# action.perform()
+    # Move the mouse to the captcha element.
+    action_chains.move_to_element(captcha_element)
 
-WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+    # Click the captcha element.
+    action_chains.click(captcha_element)
 
-searchBtn = WebDriverWait(driver, 20).until(
-    EC.element_to_be_clickable(("btnSearch", 'enabled_trigger'))
-    )
-searchBtn.click()
+    # Release the mouse button.
+    # action_chains.release()
+
+    # Perform the action chains.
+    action_chains.perform()
+    driver.switch_to.default_content()
 
 
+def login(driver, username, password):
+    # Find the username field.
+    username_field = driver.find_element("id","txtRollNo")
 
-# searchBtn = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.ID, "btnSearch"))
-#     )
-# action.click(on_element= searchBtn)
-# action.perform()
+    # Enter the username.
+    username_field.send_keys(username)
+
+    # Find the proceed button.
+    proceed_button = driver.find_element("id","btnProceed")
+
+    # Click the proceed button.
+    proceed_button.click()
+
+    # Find the password field.
+    password_field = driver.find_element("id", "txtDOB")
+
+    # Enter the password.
+    password_field.send_keys(password)
+
+    #click somewhere else
+    temp = driver.find_element(By.ID,"form1")
+    temp.click()
+
+    # Bypass the captcha.
+    bypass_captcha(driver)
+
+    # Find the login button.
+    # login_button = driver.find_element("id","btnSearch")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnSearch"))).click()
+
+    # Click the login button.
+    # login_button.click()
+
+def main():
+    # Create a new Selenium WebDriver instance.
+    driver = webdriver.Firefox()
+
+    # Open the website.
+    url = "https://erp.aktu.ac.in/webpages/oneview/oneview.aspx"
+    driver.implicitly_wait(2)
+    driver.get(url)
+
+    # Login to the website.
+    login(driver, '2200970140075', '02-07-2002')
+
+    # Close the WebDriver instance.
+    # driver.quit()
+
+if __name__ == '__main__':
+    main()
