2a8fc30888c3eba721603027b3778ccf640c4de0
unknown <nitishrawat009@gmail.com>
2023-09-26 11:22:14 +0530

    started working on ui

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    # # Find the login button.
-    # login_button = WebDriverWait(driver, 20).until(
-    # EC.element_to_be_clickable(("btnSearch", 'enabled_trigger'))
-    # )
-
-    # # Click the login button.
-    # login_button.click()
